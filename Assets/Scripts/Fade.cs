9882895e629060a6474fec4211ae09a671ea4623
ShadowVeil <nathanchadlindeman99@gmail.com>
2022-07-16 19:58:46 +0100

    material fix

--- Assets/Scripts/Fade.cs
+++ Assets/Scripts/Fade.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class Fade : MonoBehaviour
 {
+    public float duration;
     public MeshRenderer renderer;
+    public bool fade;
+
     public void OnMouseEnter()
     {
-        //mat.
+        fade = true;
     }
+
+    private void Update()
+    {
+        if(fade)
+        {
+            float lerp = Mathf.PingPong(Time.time, duration) / duration;
+            renderer.material.color = Color.Lerp(new Color(0.5f, 0.5f, 0.5f, 1f), new Color(0.5f, 0.5f, 0.5f, 0.01f), lerp);
+        }
+    }
+
 }
