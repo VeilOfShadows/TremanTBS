2ed39fe0154242cb6c29fab08c1d4f2955288980
ShadowVeil <nathanchadlindeman99@gmail.com>
2022-07-16 16:11:49 +0100

    Shader updates

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    public MeshRenderer renderer;
+    public void OnMouseEnter()
+    {
+        //mat.
+    }
+}
