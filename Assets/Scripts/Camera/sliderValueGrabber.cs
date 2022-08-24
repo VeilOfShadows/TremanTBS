5b44bfd4e688afed7f8222f45f3bfb66b2db67a8
ShadowVeil <nathanchadlindeman99@gmail.com>
2022-08-24 17:57:36 +0100

    General Fixes + TMPro import
    Shader fixes, edited the scam slider to have text display and be a bit bigger. Imported TextMeshPro into the project

--- Assets/Scripts/Camera/sliderValueGrabber.cs
+++ Assets/Scripts/Camera/sliderValueGrabber.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class sliderValueGrabber : MonoBehaviour
 {
@@ -10,12 +11,20 @@ public class sliderValueGrabber : MonoBehaviour
     public float sliderValue;
     public float finalValue;
     public GameObject cameraUpdater;
+    public TextMeshProUGUI text;
+
     public void UpdateSliderValue()
     {
         sliderValue = slider.value;
         finalValue = sliderValue + 0.5f;
         cameraUpdater.GetComponent<CameraSwapper>().transitionTimeUpdate(finalValue);
         
+    }
+
+    public void UpdateUI()
+    {
+        sliderValue = slider.value;
+        text.text = sliderValue.ToString();
     }
 
 }
